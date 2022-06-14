5ca78a0583010ad432e6149c13722455005d29dc
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-06-15 07:14:12 +0800

    改缓存配置

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -7,15 +7,19 @@ module.exports = {
 	swDest: './public/service-worker.js',
 	runtimeCaching: [
 		{
-			urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/,
-			handler: 'CacheFirst',
-		},
-		{
 			urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/,
 			handler: 'CacheFirst',
 		},
 		{
 			urlPattern: /^https:\/\/cdn\.yuameshi\.top\/.*/,
+			handler: 'CacheFirst',
+		},
+		{
+			urlPattern: /^https:\/\/blog-old\.yuameshi\.top\/.*/,
+			handler: 'CacheFirst',
+		},
+		{
+			urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/,
 			handler: 'CacheFirst',
 		},
 	],
