ad9c28037a2584d973a1639f64d62e8d99f45c95
糸 <64469437+Yuameshi@users.noreply.github.com>
2022-11-13 15:12:33 +0800

    add dns prefetch

---
+++ scripts/DnsPrefetch.js
@@ -0,0 +1,10 @@
+hexo.extend.injector.register(
+	'head_begin',
+	'<link rel="dns-prefetch" href="https://cdn.yuameshi.top/" />',
+	'default'
+);
+hexo.extend.injector.register(
+	'head_begin',
+	'<link rel="dns-prefetch" href="https://cdnjs.cloudflare.com/" />',
+	'default'
+);
